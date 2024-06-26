c01f4c22afa7b847b96ad04a8aaf766f7c98866d
Simon Ribeiro <ribeiro.simon@protonmail.com>
2024-06-26 16:02:59 +0200

    Exercice terminé. Le nombre d'étoiles est obligatoirement compris entre 1 et 5, il s'affiche dans les avis.

--- avis.js
+++ avis.js
@@ -14,9 +14,9 @@ export function ajoutListenersAvis() {
             const pieceElement = event.target.parentElement;
             const avisElement = document.createElement("p");
             avisElement.classList.toggle(`class${id}`)
-
+            
             for (let i = 0; i < avis.length; i++) {
-               avisElement.innerHTML += `<b>${avis[i].utilisateur}:</b> ${avis[i].commentaire} <br>`;
+               avisElement.innerHTML += `<b>${avis[i].utilisateur}:</b> ${avis[i].commentaire} (${avis[i].nbEtoiles}/5 étoiles)<br>`;
             }
             pieceElement.appendChild(avisElement);
             event.target.textContent = "Cacher les avis"
@@ -33,21 +33,22 @@ export function ajoutListenersAvis() {
  export function ajoutListenerEnvoyerAvis() {
     const formulaireAvis = document.querySelector(".formulaire-avis");
     formulaireAvis.addEventListener("submit", function (event) {
-    event.preventDefault();
-    // Création de l’objet du nouvel avis.
-    const avis = {
-        pieceId: parseInt(event.target.querySelector("[name=piece-id]").value),
-        utilisateur: event.target.querySelector("[name=utilisateur").value,
-        commentaire: event.target.querySelector("[name=commentaire]").value,
-    };
-    // Création de la charge utile au format JSON
-    const chargeUtile = JSON.stringify(avis);
-    // Appel de la fonction fetch avec toutes les informations nécessaires
-    fetch("http://localhost:8081/avis", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: chargeUtile
-    });
+      event.preventDefault();
+      // Création de l’objet du nouvel avis.
+      const avis = {
+         pieceId: parseInt(event.target.querySelector("[name=piece-id]").value),
+         utilisateur: event.target.querySelector("[name=utilisateur").value,
+         commentaire: event.target.querySelector("[name=commentaire]").value,
+         nbEtoiles: event.target.querySelector("[name=nbEtoiles]").value,
+      };
+      // Création de la charge utile au format JSON
+      const chargeUtile = JSON.stringify(avis);
+      // Appel de la fonction fetch avec toutes les informations nécessaires
+      fetch("http://localhost:8081/avis", {
+         method: "POST",
+         headers: { "Content-Type": "application/json" },
+         body: chargeUtile
+      });
     });
     
  }
